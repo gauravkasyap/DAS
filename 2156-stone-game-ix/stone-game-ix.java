ed41c2d1043caf3c04925bba82a4cbf44cafc879
Gaurav kumar <120582666+gauravkasyap@users.noreply.github.com>
2026-08-16 23:12:24 +0530

    Time: 4 ms (75.68%) | Memory: 114.5 MB (97.97%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int[] f={0,0,0};
+
+        for(int S:stones) f[S%3]++;
+
+        if((f[0]&1)==0) return Math.min(f[1], f[2])>0;
+
+        return Math.abs(f[1]-f[2])>2;
+    }
+}
